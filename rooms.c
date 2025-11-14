08ca745cec373d139fad8ff92dd2d32a3078a8d2
aria-dotdotdot <aria.barnes2006@gmail.com>
2025-11-14 23:33:09 +0000

    rooms.c file made

--- rooms.c
+++ rooms.c
@@ -4,6 +4,14 @@
 int rooms() {
 
     Map maps = new_map(15);
+    for(int x = 0; x < 32; x++){
+        Map.block[0][x] = '#';
+        Map.block[x][0] = '#';
+        Map.block[31][x] = '#';
+        Map.block[x][31] = '#';
+    }
+
+    
 
     return 0;
 }
